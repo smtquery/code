21a2f03ada216061d407492573018be86669d78e
Mitja Kulczynski <mku@informatik.uni-kiel.de>
2021-10-22 14:15:31 +0200

    variables

--- smtquery/storage/smt/db.py
+++ smtquery/storage/smt/db.py
@@ -103,31 +103,40 @@ class ProbeSMTFiles():
         return z3.parse_smt2_file(instance_path)
 
     def _mergeData(self,d1,d2):
-        for k in d1.keys():
-            if isinstance(d1[k],int):
-                d1[k] = d1[k]+d2[k]
-            else:
-                d1[k] = self._mergeData(d1[k],d2[k])
+        for k in set(d1.keys()).union(set(d2.keys())):
+            if k in d1 and k in d2:
+                if isinstance(d1[k],int): 
+                    d1[k] = d1[k]+d2[k]
+                else:
+                    d1[k] = self._mergeData(d1[k],d2[k])
+            elif k in d2:
+                d1[k] = d2[k]
         return d1
     
     def traverseAst(self,ast):
-        data = {"weq":0,"length":0,"regex":0,"hof":{f : 0 for f in self.hofunc}}
+        data = {"variables": dict(), "weq":0,"length":0,"regex":0,"hof":{f : 0 for f in self.hofunc}}
         if type(ast) in [z3.z3.AstVector]:
             for x in ast:
                 data = self._mergeData(data,self.traverseAst(x))
         else:
             children = ast.children()
             if len(children) == 0:
+                if str(ast.sort()) == "String":
+                    if not ast.is_string_value():
+                        x = str(ast)
+                        if x not in data["variables"]:
+                            data["variables"][x] = 0
+                        data["variables"][x]+=1
                 return data
             op = ast.decl()
+            if str(op) == "InRe":
+                data["regex"]+=1
+            elif str(op) in self.hofunc:
+                data["hof"][str(op)]+=1
             if type(ast) == z3.z3.BoolRef and len(children) == 2 and str(children[0].sort()) == "String" and children[0].sort() == children[1].sort():
                 data["weq"]+=1
             elif type(ast) == z3.z3.BoolRef and len(children) == 2 and str(children[0].sort()) == "Int" and children[0].sort() == children[1].sort():
                 data["length"]+=1
-            elif str(op) == "InRe":
-                data["regex"]+=1
-            elif str(op) in self.hofunc:
-                data["hof"][str(op)]+=1
 
             for x in children:
                 data = self._mergeData(data,self.traverseAst(x))
