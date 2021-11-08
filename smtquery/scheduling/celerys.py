434bd5e6d8cc4a73eb30c4d1dd982cff3d8b742b
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2021-11-08 11:47:01 +0100

    Result publishing to celery worker
     Addresses #2

--- smtquery/scheduling/celerys.py
+++ smtquery/scheduling/celerys.py
@@ -20,17 +20,18 @@ def setupCelery ():
         solver = smtquery.solvers.solverarr[data["solver"]]
         timeout = data["timeout"]
         split = data["smtname"].split (":")
-        track = smtquery.storage.smt.storage.searchForTrack (split[0]) 
-        if track:
-            file = track.searchFile (split[1])
+        file = smtquery.storage.smt.storage.searchFile (split[0],split[1],split[2]) 
+        if file:
             res = solver.runSolver (file,timeout,store)
+            smtquery.storage.smt.storage.storeResult (res,file,solver)
+            
             return {"result" : res.getResult ().value,
                     "time" : res.getTime (),
                     "model" : res.getModel ()
                     }
-            
+        
         return "None"
-    
+
     return app,runFunc
     
 
