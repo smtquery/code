35c1b9ae8cfb4121b39d3ec93ed4593b33cc35af
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2021-11-19 22:08:06 +0100

    Lazy loading of inteels

--- smtquery/intel/manager.py
+++ smtquery/intel/manager.py
@@ -1,3 +1,20 @@
+class Proxy:
+    def __init__ (self,cls,params):
+        self._obj = None
+        self._cls = cls
+        self._params = params
+
+    def _get (self):
+        if not self._obj:
+            self._obj = self._cls(*self._params)
+        return self._obj
+    
+    def __getattr__(self,name):
+        return self._get().__getattr__(name)
+
+    def __getitem__(self, key):
+        return self._get().__getitem__(key)
+    
 class Manager:
     def __init__(self,plugins = None):
         self._plugins = plugins or {}
@@ -7,7 +24,7 @@ class Manager:
         
     def getIntel (self,smtfile):
         for s,p in self._plugins.items():
-            setattr(smtfile,s,p.getIntel (smtfile))
+            setattr(smtfile,s,Proxy(p.getIntel,[smtfile]))
             
         return smtfile
 
