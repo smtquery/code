46c6356d80fc394c1a7fb1c8fe556d3f1d2543cb
Danny Bøgsted Poulsen <dannybpoulsen@hotmail.com>
2022-01-10 14:56:39 +0100

    Missing bing

--- smtquery/tools/__init__.py
+++ smtquery/tools/__init__.py
@@ -2,11 +2,13 @@ import smtquery.tools.qlang as qlang
 import smtquery.tools.solver as smtsolver
 import smtquery.tools.init as init
 import smtquery.tools.update_results as update
+import smtquery.tools.worker as worker
 
 
 tools = [qlang,
          smtsolver,
          init,
-         update
+         update,
+         worker
 ]
 
