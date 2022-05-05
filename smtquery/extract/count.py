2fde0d7e418dd65888014c3be590048497895234
Mitja Kulczynski <mku@informatik.uni-kiel.de>
2022-05-05 09:30:57 +0200

    count fix

--- smtquery/extract/count.py
+++ smtquery/extract/count.py
@@ -2,16 +2,19 @@ import smtquery.ui
 from smtquery.solvers.solver import *
 
 class CountInstances:
+
+    _totalCount = 0
+    
     @staticmethod
     def getName ():
         return "Count"
 
     def finalise(self,total):
         with smtquery.ui.output.makePlainMessager () as mess:
-            mess.message (f"Total matching instances: {total}")
+            mess.message (f"Total matching instances: {self._totalCount}")
         
     def __call__  (self,smtfile):
-        pass
+        self._totalCount+=1
 
 def PullExtractor():
     return [CountInstances]
